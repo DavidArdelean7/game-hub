6590ea5ccff0fdd84489f22e5638f27a4a990282
DavidArdelean7 <david.ardelean00@e-uvt.ro>
2024-02-12 15:34:45 +0200

    Ship genres and platforms with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,19 +1,20 @@
-import useData from "./useData";
+import genres from "../data/genres";
 
-interface Game{
-    id:number;
-    slug:string;
-    name:string;
+interface Game {
+  id: number;
+  slug: string;
+  name: string;
 }
 
-export interface Genre{
-    games: Game[];
-    id:number;
-    image_background:string;
-    name:string;
-    slug:string;
+export interface Genre {
+  games: Game[];
+  id: number;
+  image_background: string;
+  name: string;
+  slug: string;
 }
 
-const useGenres = ()=>useData<Genre>('/genres');
+//const useGenres = ()=>useData<Genre>('/genres');
+const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenres;
